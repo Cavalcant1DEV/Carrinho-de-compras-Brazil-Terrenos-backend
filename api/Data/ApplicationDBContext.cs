3775381a5cd6ac8877a2921d36c0b17a63b2f52e
Cavalcant1DEV <108488705+Cavalcant1DEV@users.noreply.github.com>
2026-09-15 20:06:10 -0300

    Chore: Instancia e configuração base de conexão

---
+++ api/Data/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+
+using api.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Cupom> Cupom { get; set; }
+        public DbSet<Discount> Discount { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<StockMovement> StockMovement { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
