59eb51d05e11c52673bf4ca707d9cc6a01d8d381
Cavalcant1DEV <108488705+Cavalcant1DEV@users.noreply.github.com>
2026-09-15 20:41:15 -0300

    Chore: Inserção do banco de dados
    Conexão do banco de dados em ambiente containerizado

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3775381a5cd6ac8877a2921d36c0b17a63b2f52e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a39abacb4fa4b9256c603326946240a5d2815b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
