945737d604923c55df68a00ec2a1c0c462145b0b
Cavalcant1DEV <108488705+Cavalcant1DEV@users.noreply.github.com>
2026-09-15 22:39:33 -0300

    fix: Otimização de tabelas
    Removida a tabela de userId visto que vai ser apenas uma aplicação avaliativa e não evoluirá a nível de possuir autenticação, também melhorado a forma como é registrado o desconto, deixando a mercê do frontend fazer o cálculo para display do valor total após descontos.

--- api/Models/Purchase.cs
+++ api/Models/Purchase.cs
@@ -5,11 +5,8 @@ namespace api.models
     public class Purchase
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
         [Column(TypeName = "decimal(18,2)")]
-        public decimal Value { get; set; }
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal totalDiscount { get; set; }
+        public decimal Subtotal { get; set; }
         public int? DiscountId { get; set; }
         public Discount? Discount { get; set; }
         public ICollection<StockMovement> StockMovements { get; set; } = [];
