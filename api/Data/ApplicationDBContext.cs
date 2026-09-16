efbdeba35d4a4c19ccf6253fc89ffca6ddbdc926
Cavalcant1DEV <108488705+Cavalcant1DEV@users.noreply.github.com>
2026-09-15 22:37:53 -0300

    fix: correção de nomenclatura para padronização

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -7,16 +7,13 @@ namespace api.Data
     public class ApplicationDBContext : DbContext
     {
         public ApplicationDBContext(DbContextOptions dbContextOptions)
-        : base(dbContextOptions)
-        {
-
-        }
+        : base(dbContextOptions) {}
 
         public DbSet<Cupom> Cupom { get; set; }
         public DbSet<Discount> Discount { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<Stock> Stock { get; set; }
         public DbSet<StockMovement> StockMovement { get; set; }
-        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<Purchase> Purchase { get; set; }
     }
 }
