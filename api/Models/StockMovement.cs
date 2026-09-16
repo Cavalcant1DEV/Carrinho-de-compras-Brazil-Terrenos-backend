7e2a550e567434caeb75862ddecd490824106413
Cavalcant1DEV <108488705+Cavalcant1DEV@users.noreply.github.com>
2026-09-16 10:18:46 -0300

    fix: remoção de userID
    Como discutido anteriormente, o id de usuário não será necessário visto que não haverá etapa posterior para autenticação

--- api/Models/StockMovement.cs
+++ api/Models/StockMovement.cs
@@ -6,7 +6,6 @@ namespace api.models
     public class StockMovement
     {
         public int Id { get; set; }
-        public int UserId { get; set; } = 1;
         public int? PurchaseId { get; set; }
         public Purchase? Purchase { get; set; }
         public int ProductId { get; set; }
